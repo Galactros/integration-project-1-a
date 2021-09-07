b2dd729c6ab421501c617a551bcbeb0dc4ce7138
Galactros <xtsjamal@gmail.com>
2021-09-07 10:55:37 -0300

    Add project

---
+++ src/main/java/com/galactros/puc/integrationproject1a/IntegrationProject1AApplication.java
@@ -0,0 +1,13 @@
+package com.galactros.puc.integrationproject1a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegrationProject1AApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegrationProject1AApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/galactros/puc/integrationproject1a/IntegrationProject1AApplicationTests.java
@@ -0,0 +1,13 @@
+package com.galactros.puc.integrationproject1a;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegrationProject1AApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
